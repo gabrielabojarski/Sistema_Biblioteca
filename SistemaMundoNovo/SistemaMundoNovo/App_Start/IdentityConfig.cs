4a55ec781ed33728a24f7667f4ab3bc6e949806e
AmaliaCristina <arcoresdiva@hotmail.com>
2018-09-18 10:44:17 -0300

    Deixando a senha facil

--- SistemaMundoNovo/SistemaMundoNovo/App_Start/IdentityConfig.cs
+++ SistemaMundoNovo/SistemaMundoNovo/App_Start/IdentityConfig.cs
@@ -53,11 +53,11 @@ namespace SistemaMundoNovo
             // Configurar a lógica de validação para senhas
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 3,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configurar padrões de bloqueio de usuário
