38530a8255c5cb8b9754087c9cf90e1a297fb6dc
AmaliaCristina <arcoresdiva@hotmail.com>
2018-09-18 11:05:46 -0300

    Criando UsuarioUtils

---
+++ SistemaMundoNovo/SistemaMundoNovo/Utils/UsuarioUtils.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SistemaMundoNovo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaMundoNovo.Utils
+{
+    public class UsuarioUtils
+    {
+        
+
+       public static ApplicationUser RetornaUsuarioLogado()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            string chaveUsuarioAspNet = HttpContext.Current.User.Identity.GetUserId();
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var usuarioLogado = manager.FindById(HttpContext.Current.User.Identity.GetUserId());
+
+            return usuarioLogado;
+        }
+    }
+}
